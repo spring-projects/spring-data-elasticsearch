655ccb0cec8419990171f3d01d30c5405755e648
Artur Konczak <akonczak@gmail.com>
2013-11-10 19:22:28 +0000

    DATAES-29, ISSUE #16 - formatted new files

--- src/test/java/org/springframework/data/elasticsearch/core/CustomEntityMapper.java
+++ src/test/java/org/springframework/data/elasticsearch/core/CustomEntityMapper.java
@@ -7,13 +7,19 @@ import java.io.IOException;
  */
 public class CustomEntityMapper implements EntityMapper {
 
+    public CustomEntityMapper(){
+        //custom configuration/implementation (e.g. FasterXML/jackson)
+    }
+
     @Override
     public String mapToString(Object object) throws IOException {
+        //mapping Object to String
         return null;
     }
 
     @Override
     public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
+        //mapping String to Object
         return null;
     }
 }
