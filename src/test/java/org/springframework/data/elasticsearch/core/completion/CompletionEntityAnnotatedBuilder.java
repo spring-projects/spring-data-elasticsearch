dfa0823c9a45f10d8ce2d838da5c98458abfa0c9
Felix Peters <peters@develop4edu.de>
2014-11-11 16:46:57 +0100

    DATAES-91 - completion suggester support

---
+++ src/test/java/org/springframework/data/elasticsearch/core/completion/CompletionEntityAnnotatedBuilder.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.elasticsearch.core.completion;
+
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+
+/**
+ * @author Franck Marchand
+ * @author Mohsin Husen
+ * @author Mewes Kochheim
+ */
+public class CompletionEntityAnnotatedBuilder {
+
+    private CompletionAnnotatedEntity result;
+
+    public CompletionEntityAnnotatedBuilder(String id) {
+	result = new CompletionAnnotatedEntity(id);
+    }
+
+    public CompletionEntityAnnotatedBuilder name(String name) {
+	result.setName(name);
+	return this;
+    }
+
+    public CompletionEntityAnnotatedBuilder suggest(String[] input) {
+	return suggest(input, null, null, null);
+    }
+
+    public CompletionEntityAnnotatedBuilder suggest(String[] input, String output) {
+	return suggest(input, output, null, null);
+    }
+
+    public CompletionEntityAnnotatedBuilder suggest(String[] input, String output, Object payload) {
+	return suggest(input, output, payload, null);
+    }
+
+    public CompletionEntityAnnotatedBuilder suggest(String[] input, String output, Object payload, Integer weight) {
+	Completion suggest = new Completion(input);
+	if (output != null) {
+	    suggest.setOutput(output);
+	}
+	if (payload != null) {
+	    suggest.setPayload(payload);
+	}
+	if (weight != null) {
+	    suggest.setWeight(weight);
+	}
+	result.setSuggest(suggest);
+	return this;
+    }
+
+    public CompletionAnnotatedEntity build() {
+	return result;
+    }
+
+    public IndexQuery buildIndex() {
+	IndexQuery indexQuery = new IndexQuery();
+	indexQuery.setId(result.getId());
+	indexQuery.setObject(result);
+	return indexQuery;
+    }
+}
