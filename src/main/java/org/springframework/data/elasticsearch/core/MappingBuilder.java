e5abf5906e72a48af1e0d3d6783e500ff816014d
dianandonov <dandonov@sharecare.com>
2015-06-12 15:04:13 -0400

    Added support for "geohash_prefix" and "geohash_precision" on GeoPointField annotation

--- src/main/java/org/springframework/data/elasticsearch/core/MappingBuilder.java
+++ src/main/java/org/springframework/data/elasticsearch/core/MappingBuilder.java
@@ -65,6 +65,8 @@ class MappingBuilder {
 	public static final String TYPE_VALUE_STRING = "string";
 	public static final String TYPE_VALUE_GEO_POINT = "geo_point";
 	public static final String TYPE_VALUE_COMPLETION = "completion";
+	public static final String TYPE_VALUE_GEO_HASH_PREFIX = "geohash_prefix" ;
+	public static final String TYPE_VALUE_GEO_HASH_PRECISION = "geohash_precision";
 
 	private static SimpleTypeHolder SIMPLE_TYPE_HOLDER = new SimpleTypeHolder();
 
@@ -169,8 +171,18 @@ class MappingBuilder {
 
 	private static void applyGeoPointFieldMapping(XContentBuilder xContentBuilder, java.lang.reflect.Field field) throws IOException {
 		xContentBuilder.startObject(field.getName());
-		xContentBuilder.field(FIELD_TYPE, TYPE_VALUE_GEO_POINT)
-				.endObject();
+		xContentBuilder.field(FIELD_TYPE, TYPE_VALUE_GEO_POINT);
+
+		GeoPointField annotation = field.getAnnotation(GeoPointField.class);
+		if (annotation != null) {
+			if (annotation.geoHashPrefix())
+				xContentBuilder.field(TYPE_VALUE_GEO_HASH_PREFIX, true);
+
+			if ("" .equals(annotation.geoHashPrecision()))
+				xContentBuilder.field(TYPE_VALUE_GEO_HASH_PRECISION, annotation.geoHashPrecision());
+		}
+
+		xContentBuilder.endObject();
 	}
 
 	private static void applyCompletionFieldMapping(XContentBuilder xContentBuilder, java.lang.reflect.Field field, CompletionField annotation) throws IOException {
