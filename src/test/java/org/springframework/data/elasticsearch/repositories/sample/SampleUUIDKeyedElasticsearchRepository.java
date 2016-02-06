063d8ed9836e064e60128f1334634d704ba8f484
Gad Akuka <gad.akuka@sap.com>
2016-02-06 22:01:55 +0200

    DATAES-163 - Add support for UUID as primary key
    Add support for UUID as primary key
    
    Add tests for support of UUID as primary key

---
+++ src/test/java/org/springframework/data/elasticsearch/repositories/sample/SampleUUIDKeyedElasticsearchRepository.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.elasticsearch.repositories.sample;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.elasticsearch.entities.SampleEntityUUIDKeyed;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * @author Gad Akuka
+ */
+public interface SampleUUIDKeyedElasticsearchRepository extends ElasticsearchRepository<SampleEntityUUIDKeyed, UUID> {
+
+	long deleteById(UUID id);
+	List<SampleEntityUUIDKeyed> deleteByAvailable(boolean available);
+	List<SampleEntityUUIDKeyed> deleteByMessage(String message);
+	void deleteByType(String type);
+
+}
