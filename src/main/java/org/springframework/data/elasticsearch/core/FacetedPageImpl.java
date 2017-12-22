9b23b2bba5ed3b64b935e69361ad8862052769ff
ske <SKERMANI@dimosoftware.com>
2017-12-22 11:36:30 +0100

    Correction de la pagination

--- src/main/java/org/springframework/data/elasticsearch/core/FacetedPageImpl.java
+++ src/main/java/org/springframework/data/elasticsearch/core/FacetedPageImpl.java
@@ -60,7 +60,7 @@ public abstract class FacetedPageImpl<T> extends PageImpl<T> implements FacetedP
 	}
 
 	public FacetedPageImpl(List<T> content, Pageable pageable, long total) {
-		super(content, Pageable.unpaged(), total);
+		super(content, pageable, total);
 	}
 
 	@Override
