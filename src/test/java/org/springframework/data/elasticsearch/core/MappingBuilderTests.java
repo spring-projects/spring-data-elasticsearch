faabaf0156685506c135cae6a6734e58c8e48249
Don Wellington <dastardly740@yahoo.com>
2018-06-14 22:58:31 -0700

    DATAES-468 Update to 6.3.0

--- src/test/java/org/springframework/data/elasticsearch/core/MappingBuilderTests.java
+++ src/test/java/org/springframework/data/elasticsearch/core/MappingBuilderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 the original author or authors.
+ * Copyright 2013-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.springframework.data.elasticsearch.utils.IndexBuilder.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
@@ -45,13 +46,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Mohsin Husen
  * @author Keivn Leturc
  * @author Nordine Bittich
+ * @author Don Wellington
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:elasticsearch-template-test.xml")
 public class MappingBuilderTests {
 
-	@Autowired
-	private ElasticsearchTemplate elasticsearchTemplate;
+	@Autowired private ElasticsearchTemplate elasticsearchTemplate;
+
+	private String xContentBuilderToString(XContentBuilder builder) {
+		builder.close();
+		ByteArrayOutputStream bos = (ByteArrayOutputStream) builder.getOutputStream();
+		return bos.toString();
+	}
 
 	@Test
 	public void shouldNotFailOnCircularReference() {
@@ -63,43 +70,43 @@ public class MappingBuilderTests {
 
 	@Test
 	public void testInfiniteLoopAvoidance() throws IOException {
-		final String expected = "{\"mapping\":{\"properties\":{\"message\":{\"store\":true,\"" +
-				"type\":\"text\",\"index\":false," +
-				"\"analyzer\":\"standard\"}}}}";
+		final String expected = "{\"mapping\":{\"properties\":{\"message\":{\"store\":true,\""
+				+ "type\":\"text\",\"index\":false," + "\"analyzer\":\"standard\"}}}}";
 
 		XContentBuilder xContentBuilder = MappingBuilder.buildMapping(SampleTransientEntity.class, "mapping", "id", null);
-		assertThat(xContentBuilder.string(), is(expected));
+		assertThat(xContentBuilderToString(xContentBuilder), is(expected));
 	}
 
 	@Test
 	public void shouldUseValueFromAnnotationType() throws IOException {
-		//Given
+		// Given
 		final String expected = "{\"mapping\":{\"properties\":{\"price\":{\"store\":false,\"type\":\"double\"}}}}";
 
-		//When
+		// When
 		XContentBuilder xContentBuilder = MappingBuilder.buildMapping(StockPrice.class, "mapping", "id", null);
 
-		//Then
-		assertThat(xContentBuilder.string(), is(expected));
+		// Then
+		assertThat(xContentBuilderToString(xContentBuilder), is(expected));
 	}
 
 	@Test
 	public void shouldAddStockPriceDocumentToIndex() throws IOException {
-		//Given
+		// Given
 
-		//When
+		// When
 		elasticsearchTemplate.deleteIndex(StockPrice.class);
 		elasticsearchTemplate.createIndex(StockPrice.class);
 		elasticsearchTemplate.putMapping(StockPrice.class);
 		String symbol = "AU";
 		double price = 2.34;
 		String id = "abc";
-		elasticsearchTemplate.index(buildIndex(StockPrice.builder().id(id).symbol(symbol).price(new BigDecimal(price)).build()));
+		elasticsearchTemplate
+				.index(buildIndex(StockPrice.builder().id(id).symbol(symbol).price(new BigDecimal(price)).build()));
 		elasticsearchTemplate.refresh(StockPrice.class);
 
 		SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(matchAllQuery()).build();
 		List<StockPrice> result = elasticsearchTemplate.queryForList(searchQuery, StockPrice.class);
-		//Then
+		// Then
 		assertThat(result.size(), is(1));
 		StockPrice entry = result.get(0);
 		assertThat(entry.getSymbol(), is(symbol));
@@ -110,7 +117,7 @@ public class MappingBuilderTests {
 	public void shouldCreateMappingForSpecifiedParentType() throws IOException {
 		final String expected = "{\"mapping\":{\"_parent\":{\"type\":\"parentType\"},\"properties\":{}}}";
 		XContentBuilder xContentBuilder = MappingBuilder.buildMapping(MinimalEntity.class, "mapping", "id", "parentType");
-		assertThat(xContentBuilder.string(), is(expected));
+		assertThat(xContentBuilderToString(xContentBuilder), is(expected));
 	}
 
 	/*
@@ -118,13 +125,12 @@ public class MappingBuilderTests {
 	 */
 	@Test
 	public void shouldBuildMappingWithSuperclass() throws IOException {
-		final String expected = "{\"mapping\":{\"properties\":{\"message\":{\"store\":true,\"" +
-				"type\":\"text\",\"index\":false,\"analyzer\":\"standard\"}" +
-				",\"createdDate\":{\"store\":false," +
-				"\"type\":\"date\",\"index\":false}}}}";
+		final String expected = "{\"mapping\":{\"properties\":{\"message\":{\"store\":true,\""
+				+ "type\":\"text\",\"index\":false,\"analyzer\":\"standard\"}" + ",\"createdDate\":{\"store\":false,"
+				+ "\"type\":\"date\",\"index\":false}}}}";
 
 		XContentBuilder xContentBuilder = MappingBuilder.buildMapping(SampleInheritedEntity.class, "mapping", "id", null);
-		assertThat(xContentBuilder.string(), is(expected));
+		assertThat(xContentBuilderToString(xContentBuilder), is(expected));
 	}
 
 	/*
@@ -132,21 +138,22 @@ public class MappingBuilderTests {
 	 */
 	@Test
 	public void shouldAddSampleInheritedEntityDocumentToIndex() throws IOException {
-		//Given
+		// Given
 
-		//When
+		// When
 		elasticsearchTemplate.deleteIndex(SampleInheritedEntity.class);
 		elasticsearchTemplate.createIndex(SampleInheritedEntity.class);
 		elasticsearchTemplate.putMapping(SampleInheritedEntity.class);
 		Date createdDate = new Date();
 		String message = "msg";
 		String id = "abc";
-		elasticsearchTemplate.index(new SampleInheritedEntityBuilder(id).createdDate(createdDate).message(message).buildIndex());
+		elasticsearchTemplate
+				.index(new SampleInheritedEntityBuilder(id).createdDate(createdDate).message(message).buildIndex());
 		elasticsearchTemplate.refresh(SampleInheritedEntity.class);
 
 		SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(matchAllQuery()).build();
 		List<SampleInheritedEntity> result = elasticsearchTemplate.queryForList(searchQuery, SampleInheritedEntity.class);
-		//Then
+		// Then
 		assertThat(result.size(), is(1));
 		SampleInheritedEntity entry = result.get(0);
 		assertThat(entry.getCreatedDate(), is(createdDate));
@@ -155,13 +162,13 @@ public class MappingBuilderTests {
 
 	@Test
 	public void shouldBuildMappingsForGeoPoint() throws IOException {
-		//given
+		// given
 
-		//when
+		// when
 		XContentBuilder xContentBuilder = MappingBuilder.buildMapping(GeoEntity.class, "mapping", "id", null);
 
-		//then
-		final String result = xContentBuilder.string();
+		// then
+		final String result = xContentBuilderToString(xContentBuilder);
 
 		assertThat(result, containsString("\"pointA\":{\"type\":\"geo_point\""));
 		assertThat(result, containsString("\"pointB\":{\"type\":\"geo_point\""));
@@ -174,40 +181,40 @@ public class MappingBuilderTests {
 	 */
 	@Test
 	public void shouldHandleReverseRelationship() {
-		//given
+		// given
 		elasticsearchTemplate.createIndex(User.class);
 		elasticsearchTemplate.putMapping(User.class);
 		elasticsearchTemplate.createIndex(Group.class);
 		elasticsearchTemplate.putMapping(Group.class);
-		//when
+		// when
 
-		//then
+		// then
 
 	}
 
 	@Test
 	public void shouldMapBooks() {
-		//given
+		// given
 		elasticsearchTemplate.createIndex(Book.class);
 		elasticsearchTemplate.putMapping(Book.class);
-		//when
-		//then
+		// when
+		// then
 
 	}
 
 	@Test // DATAES-420
 	public void shouldUseBothAnalyzer() {
-		//given
+		// given
 		elasticsearchTemplate.deleteIndex(Book.class);
 		elasticsearchTemplate.createIndex(Book.class);
 		elasticsearchTemplate.putMapping(Book.class);
 
-		//when
+		// when
 		Map mapping = elasticsearchTemplate.getMapping(Book.class);
 		Map descriptionMapping = (Map) ((Map) mapping.get("properties")).get("description");
 		Map prefixDescription = (Map) ((Map) descriptionMapping.get("fields")).get("prefix");
 
-		//then
+		// then
 		assertThat(prefixDescription.size(), is(3));
 		assertThat(prefixDescription.get("type"), equalTo("text"));
 		assertThat(prefixDescription.get("analyzer"), equalTo("stop"));
