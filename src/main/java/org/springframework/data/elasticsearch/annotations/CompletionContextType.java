365b0c47d8879b4d31f125f74d1af960424b9836
Robert Gründler <robert@dubture.com>
2019-02-21 09:04:19 +0100

    DATAES-536 - Add support for context suggester
    Original pull request: #241

---
+++ src/main/java/org/springframework/data/elasticsearch/annotations/CompletionContextType.java
@@ -0,0 +1,12 @@
+package org.springframework.data.elasticsearch.annotations;
+
+/**
+ * Based on reference doc - https://www.elastic.co/guide/en/elasticsearch/reference/current/suggester-context.html
+ *
+ * @author Robert Gruendler
+ */
+public enum CompletionContextType {
+
+	CATEGORY, GEO
+
+}
