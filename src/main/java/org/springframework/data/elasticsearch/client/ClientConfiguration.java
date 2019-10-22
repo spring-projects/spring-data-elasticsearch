f7f103d331903a120433cee8a08edd0309bee5b8
Henrique Amaral <hen.amaral@gmail.com>
2019-10-22 19:02:19 +0200

    DATAES-673 Create a Ssl Rest Client using SslContext and HostnameVerifier
    Original PR: #334

--- src/main/java/org/springframework/data/elasticsearch/client/ClientConfiguration.java
+++ src/main/java/org/springframework/data/elasticsearch/client/ClientConfiguration.java
@@ -21,6 +21,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 
 import org.springframework.http.HttpHeaders;
@@ -31,6 +32,7 @@ import org.springframework.http.HttpHeaders;
  * @author Mark Paluch
  * @author Peter-Josef Meisch
  * @author Huw Ayling-Miller
+ * @author Henrique Amaral
  * @since 3.2
  */
 public interface ClientConfiguration {
@@ -118,6 +120,13 @@ public interface ClientConfiguration {
 	 * @return the {@link SSLContext} to use. Can be {@link Optional#empty()} if unconfigured.
 	 */
 	Optional<SSLContext> getSslContext();
+
+	/**
+	 * Returns the {@link HostnameVerifier} to use. Can be {@link Optional#empty()} if unconfigured.
+	 *
+	 * @return the {@link HostnameVerifier} to use. Can be {@link Optional#empty()} if unconfigured.
+	 */
+	Optional<HostnameVerifier> getHostNameVerifier();
 
 	/**
 	 * Returns the {@link java.time.Duration connect timeout}.
@@ -210,6 +219,16 @@ public interface ClientConfiguration {
 		 * @return the {@link TerminalClientConfigurationBuilder}.
 		 */
 		TerminalClientConfigurationBuilder usingSsl(SSLContext sslContext);
+
+		/**
+		 * Connect via {@literal https} using the givens {@link SSLContext} and HostnameVerifier {@link HostnameVerifier}  .<br />
+		 *
+		 * <strong>NOTE</strong> You need to leave out the protocol in
+		 * {@link ClientConfigurationBuilderWithRequiredEndpoint#connectedTo(String)}.
+		 *
+		 * @return the {@link TerminalClientConfigurationBuilder}.
+		 */
+		TerminalClientConfigurationBuilder usingSsl(SSLContext sslContext, HostnameVerifier hostnameVerifier);
 	}
 
 	/**
