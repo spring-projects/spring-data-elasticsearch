bcf64b0c74899695d5ebcd0557a3214370255f4b
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-04-27 16:38:52 +0400

    DATAES-799 - Support optimistic locking for full update scenario using seq_no + primary_term.
    Add documentation

--- src/main/java/org/springframework/data/elasticsearch/core/mapping/SeqNoPrimaryTerm.java
+++ src/main/java/org/springframework/data/elasticsearch/core/mapping/SeqNoPrimaryTerm.java
@@ -1,8 +1,39 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.elasticsearch.core.mapping;
 
 import org.springframework.lang.Nullable;
 
 /**
+ * <p>A container for seq_no and primary_term values. When an entity class contains a field of this type,
+ * it will be automatically filled with SeqNoPrimaryTerm instance on read operations (like get or search),
+ * and also, when the SeqNoPrimaryTerm is not {@literal null} and filled with seq_no and primary_term,
+ * they will be sent to Elasticsearch when indexing such an entity.
+ * </p>
+ * <p>This allows to implement optimistic locking pattern for full-update scenario, when an entity is first
+ * read from Elasticsearch and then gets reindexed with new _content.
+ * Index operations will throw an {@link org.springframework.dao.OptimisticLockingFailureException} if the
+ * seq_no + primary_term pair already has different values for the given document. See Elasticsearch documentation
+ * for more information: https://www.elastic.co/guide/en/elasticsearch/reference/current/optimistic-concurrency-control.html
+ * </p>
+ * <p>
+ * A property of this type is implicitly @{@link org.springframework.data.annotation.Transient} and never gets included
+ * into a mapping at Elasticsearch side.
+ * </p>
+ *
  * @author Roman Puchkovskiy
  * @since 4.0
  */
