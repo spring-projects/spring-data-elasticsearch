887e35bb2ef1b4e7588b82d422466509705911a3
Tim te Beek <tim.te.beek@jdriven.com>
2020-05-29 17:27:06 +0200

    Add DateFormat values for epoch_millis and epoch_second

--- src/main/java/org/springframework/data/elasticsearch/annotations/DateFormat.java
+++ src/main/java/org/springframework/data/elasticsearch/annotations/DateFormat.java
@@ -24,8 +24,8 @@ public enum DateFormat {
 	basic_ordinal_date_time_no_millis, basic_time, basic_time_no_millis, basic_t_time, basic_t_time_no_millis,
 	basic_week_date, basic_week_date_time, basic_week_date_time_no_millis, date, date_hour, date_hour_minute,
 	date_hour_minute_second, date_hour_minute_second_fraction, date_hour_minute_second_millis, date_optional_time,
-	date_time, date_time_no_millis, hour, hour_minute, hour_minute_second, hour_minute_second_fraction,
-	hour_minute_second_millis, ordinal_date, ordinal_date_time, ordinal_date_time_no_millis, time, time_no_millis,
-	t_time, t_time_no_millis, week_date, week_date_time, weekDateTimeNoMillis, week_year, weekyearWeek,
-	weekyearWeekDay, year, year_month, year_month_day
+	date_time, date_time_no_millis, epoch_millis, epoch_second, hour, hour_minute, hour_minute_second,
+	hour_minute_second_fraction, hour_minute_second_millis, ordinal_date, ordinal_date_time,
+	ordinal_date_time_no_millis, time, time_no_millis, t_time, t_time_no_millis, week_date, week_date_time,
+	weekDateTimeNoMillis, week_year, weekyearWeek, weekyearWeekDay, year, year_month, year_month_day
 }
