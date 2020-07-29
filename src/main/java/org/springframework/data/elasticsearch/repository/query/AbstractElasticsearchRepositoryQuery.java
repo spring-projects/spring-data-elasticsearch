4f29f0d60c3393df1c98e70acb5bd81b0f7b3fd3
Peter-Josef Meisch <pj.meisch@sothawo.com>
2020-07-29 09:49:55 +0200

    DATAES-891 - Returning a Stream from a Query annotated repository method crashes.
    Original PR: #497
    
    (cherry picked from commit f989cf873b0e2a5e60044ffa1af42b77b05e9012)

--- src/main/java/org/springframework/data/elasticsearch/repository/query/AbstractElasticsearchRepositoryQuery.java
+++ src/main/java/org/springframework/data/elasticsearch/repository/query/AbstractElasticsearchRepositoryQuery.java
@@ -28,11 +28,12 @@ import org.springframework.data.repository.query.RepositoryQuery;
 
 public abstract class AbstractElasticsearchRepositoryQuery implements RepositoryQuery {
 
+	protected static final int DEFAULT_STREAM_BATCH_SIZE = 500;
 	protected ElasticsearchQueryMethod queryMethod;
 	protected ElasticsearchOperations elasticsearchOperations;
 
 	public AbstractElasticsearchRepositoryQuery(ElasticsearchQueryMethod queryMethod,
-												ElasticsearchOperations elasticsearchOperations) {
+			ElasticsearchOperations elasticsearchOperations) {
 		this.queryMethod = queryMethod;
 		this.elasticsearchOperations = elasticsearchOperations;
 	}
