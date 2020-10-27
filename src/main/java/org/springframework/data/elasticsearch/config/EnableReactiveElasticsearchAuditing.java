9b28955ae86338facc083c390681a11fab812111
Peter-Josef Meisch <pj.meisch@sothawo.com>
2020-10-27 12:34:58 +0100

    DATAES-962 - Deprecate Jodatime support.

--- src/main/java/org/springframework/data/elasticsearch/config/EnableReactiveElasticsearchAuditing.java
+++ src/main/java/org/springframework/data/elasticsearch/config/EnableReactiveElasticsearchAuditing.java
@@ -65,6 +65,8 @@ public @interface EnableReactiveElasticsearchAuditing {
 	 * used for setting creation and modification dates.
 	 *
 	 * @return
+	 * @deprecated since 4.1
 	 */
+	@Deprecated
 	String dateTimeProviderRef() default "";
 }
