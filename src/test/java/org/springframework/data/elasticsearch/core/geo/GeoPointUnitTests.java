fcb8a9b0ed770d46bbc0067855ba0e34cc1ef030
Peter-Josef Meisch <pj.meisch@sothawo.com>
2020-12-06 12:08:42 +0100

    DATAES-994 Add setup for mutation testing.
    Original PR: #568

---
+++ src/test/java/org/springframework/data/elasticsearch/core/geo/GeoPointUnitTests.java
@@ -0,0 +1,51 @@
+package org.springframework.data.elasticsearch.core.geo;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.geo.Point;
+
+/**
+ * @author P.J. Meisch (pj.meisch@sothawo.com)
+ */
+class GeoPointUnitTests {
+
+	@Test
+	@DisplayName("should create From a Point")
+	void shouldCreateFromAPoint() {
+		Point point = new Point(8, 48);
+
+		GeoPoint geoPoint = GeoPoint.fromPoint(point);
+
+		assertThat(geoPoint).isEqualTo(new GeoPoint(48, 8));
+	}
+
+	@Test
+	@DisplayName("should convert to a Point")
+	void shouldConvertToAPoint() {
+
+		GeoPoint geoPoint = new GeoPoint(48, 8);
+
+		Point point = GeoPoint.toPoint(geoPoint);
+
+		assertThat(point).isEqualTo(new Point(8, 48));
+	}
+
+	@Test
+	@DisplayName("should not be equal to a Point")
+	void shouldNotBeEqualToAPoint() {
+
+		//noinspection AssertBetweenInconvertibleTypes
+		assertThat(new GeoPoint(48, 8)).isNotEqualTo(new Point(8, 48));
+	}
+
+	@Test
+	@DisplayName("should compare lat and lon values")
+	void shouldCompareLatAndLonValues() {
+
+		GeoPoint geoPoint = new GeoPoint(48, 8);
+
+		assertThat(geoPoint).isEqualTo(new GeoPoint(48, 8));
+	}
+}
