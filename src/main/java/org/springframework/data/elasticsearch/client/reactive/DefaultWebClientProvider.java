4829b07e53fcbea4b391a6688fd70a580f5a62ab
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-01-25 21:33:25 +0100

    ReactiveElasticsearchOperations indexName is encoded twice.
    Original Pull Request #1666 
    Closes #1665

--- src/main/java/org/springframework/data/elasticsearch/client/reactive/DefaultWebClientProvider.java
+++ src/main/java/org/springframework/data/elasticsearch/client/reactive/DefaultWebClientProvider.java
@@ -27,6 +27,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
  * Default {@link WebClientProvider} that uses cached {@link WebClient} instances per {@code hostAndPort}.
@@ -155,8 +156,13 @@ class DefaultWebClientProvider implements WebClientProvider {
 
 		String baseUrl = String.format("%s://%s:%d%s", this.scheme, socketAddress.getHostString(), socketAddress.getPort(),
 				pathPrefix == null ? "" : '/' + pathPrefix);
+
+		DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(baseUrl);
+		// the template will already be encoded by the RequestConverters methods
+		uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+		builder.uriBuilderFactory(uriBuilderFactory); //
+
 		WebClient webClient = builder //
-				.baseUrl(baseUrl) //
 				.filter((request, next) -> next.exchange(request) //
 						.doOnError(errorListener)) //
 				.build(); //
