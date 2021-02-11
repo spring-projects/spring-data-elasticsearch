719bcf4bfc5d32ad7ca7998c43378fba8ccbf624
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-02-11 18:55:57 +0100

    Fix remaining copyright years

--- src/main/java/org/springframework/data/elasticsearch/annotations/Routing.java
+++ src/main/java/org/springframework/data/elasticsearch/annotations/Routing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2020-2021the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.springframework.data.annotation.Persistent;
 
 /**
  * Annotation to enable custom routing values for an entity.
- * 
+ *
  * @author Peter-Josef Meisch
  * @since 4.2
  */
