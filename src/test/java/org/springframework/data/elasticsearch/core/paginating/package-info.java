5120483d11a9d37bf833c89732e1cf87421d5d57
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-02-11 18:52:34 +0100

    Add support for search_after.

---
+++ src/test/java/org/springframework/data/elasticsearch/core/paginating/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Test for paginating support with search_after and point_in_time API
+ */
+@org.springframework.lang.NonNullApi
+@org.springframework.lang.NonNullFields
+package org.springframework.data.elasticsearch.core.paginating;
