84b441eadcfb1bdfaedc745e013b7571cc41bcb8
Greg L. Turnquist <gturnquist@vmware.com>
2021-04-20 10:40:58 -0500

    Authenticate with artifactory.
    See #1750.

--- ci/clean.sh
+++ ci/clean.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
-  ./mvnw clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch
+  ./mvnw -s settings.xml clean -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch
--- ci/verify.sh
+++ ci/verify.sh
@@ -6,5 +6,5 @@ mkdir -p /tmp/jenkins-home/.m2/spring-data-elasticsearch
 chown -R 1001:1001 .
 
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
-  ./mvnw \
+  ./mvnw -s settings.xml \
   -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch
