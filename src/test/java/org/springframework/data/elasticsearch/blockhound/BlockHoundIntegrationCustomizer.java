170648d467614d95602497fdfc5a9728c2fcd5fa
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-12-25 14:33:11 +0100

    Fix Blockhound setup for Elasticsearch 7.16, dependency version updates.
    Original Pull Request #2038

--- src/test/java/org/springframework/data/elasticsearch/blockhound/BlockHoundIntegrationCustomizer.java
+++ src/test/java/org/springframework/data/elasticsearch/blockhound/BlockHoundIntegrationCustomizer.java
@@ -27,10 +27,14 @@ public class BlockHoundIntegrationCustomizer implements BlockHoundIntegration {
 	@Override
 	public void applyTo(BlockHound.Builder builder) {
 		// Elasticsearch classes reading from the classpath on initialization, needed for parsing Elasticsearch responses
-		builder.allowBlockingCallsInside("org.elasticsearch.common.xcontent.XContentBuilder", "<clinit>")
+		builder //
+				.allowBlockingCallsInside("org.elasticsearch.common.xcontent.XContentBuilder", "<clinit>") // pre 7.16
+				.allowBlockingCallsInside("org.elasticsearch.common.XContentBuilder", "<clinit>") // from 7.16 on
+				.allowBlockingCallsInside("org.elasticsearch.xcontent.json.JsonXContent", "contentBuilder") // from 7.16 on
 				.allowBlockingCallsInside("org.elasticsearch.Build", "<clinit>");
 
 		builder.blockingMethodCallback(it -> {
+			new Error(it.toString()).printStackTrace();
 			throw new BlockingOperationError(it);
 		});
 
