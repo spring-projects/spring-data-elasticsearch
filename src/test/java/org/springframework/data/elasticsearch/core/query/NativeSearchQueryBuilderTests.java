5875bf3b9bd6babe7cdc1fed9c6a238b37e34fc3
Peter-Josef Meisch <pj.meisch@sothawo.com>
2022-03-12 11:33:57 +0100

    Polishing.

--- src/test/java/org/springframework/data/elasticsearch/core/query/NativeSearchQueryBuilderTests.java
+++ src/test/java/org/springframework/data/elasticsearch/core/query/NativeSearchQueryBuilderTests.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.elasticsearch.core.query;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
@@ -10,38 +25,39 @@ import com.google.common.collect.Lists;
 
 /**
  * @author owen.qq
+ * @since 4.4
  */
 public class NativeSearchQueryBuilderTests {
 
-    @Test // #2105
-    void shouldContainEffectiveSearchAfterValue() {
-        Long lastSortValue = 1L;
-        List<Object> searchAfter = Lists.newArrayList(lastSortValue);
+	@Test // #2105
+	void shouldContainEffectiveSearchAfterValue() {
+		Long lastSortValue = 1L;
+		List<Object> searchAfter = Lists.newArrayList(lastSortValue);
 
-        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-        nativeSearchQueryBuilder.withSearchAfter(searchAfter);
-        NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
+		NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+		nativeSearchQueryBuilder.withSearchAfter(searchAfter);
+		NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
 
-        assertThat(nativeSearchQuery.getSearchAfter()).isNotNull();
-    }
+		assertThat(nativeSearchQuery.getSearchAfter()).isNotNull();
+	}
 
-    @Test // #2105
-    void shouldIgnoreNullableSearchAfterValue() {
-        List<Object> emptySearchValueByFirstSearch = null;
-        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-        nativeSearchQueryBuilder.withSearchAfter(emptySearchValueByFirstSearch);
-        NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
+	@Test // #2105
+	void shouldIgnoreNullableSearchAfterValue() {
+		List<Object> emptySearchValueByFirstSearch = null;
+		NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+		nativeSearchQueryBuilder.withSearchAfter(emptySearchValueByFirstSearch);
+		NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
 
-        assertThat(nativeSearchQuery.getSearchAfter()).isNull();
-    }
+		assertThat(nativeSearchQuery.getSearchAfter()).isNull();
+	}
 
-    @Test // #2105
-    void shouldIgnoreEmptySearchAfterValue() {
-        List<Object> emptySearchValueByFirstSearch = Lists.newArrayList();
-        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-        nativeSearchQueryBuilder.withSearchAfter(emptySearchValueByFirstSearch);
-        NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
+	@Test // #2105
+	void shouldIgnoreEmptySearchAfterValue() {
+		List<Object> emptySearchValueByFirstSearch = Lists.newArrayList();
+		NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+		nativeSearchQueryBuilder.withSearchAfter(emptySearchValueByFirstSearch);
+		NativeSearchQuery nativeSearchQuery = nativeSearchQueryBuilder.build();
 
-        assertThat(nativeSearchQuery.getSearchAfter()).isNull();
-    }
+		assertThat(nativeSearchQuery.getSearchAfter()).isNull();
+	}
 }
