f90138076659e5452fd38444c85cfee0745b074c
Peter-Josef Meisch <pj.meisch@sothawo.com>
2022-06-25 19:55:22 +0200

    Fix updatebyquery request.
    Original Pull Request #2197
    Closes #2191

--- src/main/java/org/springframework/data/elasticsearch/client/erhlc/RequestFactory.java
+++ src/main/java/org/springframework/data/elasticsearch/client/erhlc/RequestFactory.java
@@ -1096,8 +1096,7 @@ class RequestFactory {
 
 	public UpdateByQueryRequest updateByQueryRequest(UpdateQuery query, IndexCoordinates index) {
 
-		String indexName = index.getIndexName();
-		final UpdateByQueryRequest updateByQueryRequest = new UpdateByQueryRequest(indexName);
+		final UpdateByQueryRequest updateByQueryRequest = new UpdateByQueryRequest(index.getIndexNames());
 		updateByQueryRequest.setScript(getScript(query));
 
 		if (query.getAbortOnVersionConflict() != null) {
