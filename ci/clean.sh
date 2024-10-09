d8917f1cb1b439ed94ed108725d28832ba62b73b
Mark Paluch <mark.paluch@broadcom.com>
2024-10-09 09:02:07 +0200

    Consistently run all CI steps with the same user.
    See #2982

--- ci/clean.sh
+++ ci/clean.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 export JENKINS_USER=${JENKINS_USER_NAME}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
-  ./mvnw -s settings.xml clean -Dscan=false -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch
+  ./mvnw -s settings.xml clean -Dscan=false -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
--- ci/verify.sh
+++ ci/verify.sh
@@ -3,9 +3,8 @@
 set -euo pipefail
 
 mkdir -p /tmp/jenkins-home/.m2/spring-data-elasticsearch
-chown -R 1001:1001 .
 export JENKINS_USER=${JENKINS_USER_NAME}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml \
-  -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch
+  -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-elasticsearch -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
