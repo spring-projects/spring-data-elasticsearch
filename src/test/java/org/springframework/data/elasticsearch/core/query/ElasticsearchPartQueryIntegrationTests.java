d7abbc719fe3bcf2c1ae9c6ebcbcceffe347313b
Peter-Josef Meisch <pj.meisch@sothawo.com>
2025-03-09 12:31:59 +0100

    Fix mapping of property names in sort parameters.
    Original Pull Request #3074
    Closes #3072
    
    Signed-off-by: Peter-Josef Meisch <pj.meisch@sothawo.com>
    (cherry picked from commit 42383624eadc79cdee0f564ad30c89d3eb486ae2)
    (cherry picked from commit 88e2e9da2aeefff178d305d867a553b6345a914c)

--- src/test/java/org/springframework/data/elasticsearch/core/query/ElasticsearchPartQueryIntegrationTests.java
+++ src/test/java/org/springframework/data/elasticsearch/core/query/ElasticsearchPartQueryIntegrationTests.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.json.JSONException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
@@ -639,6 +640,45 @@ public abstract class ElasticsearchPartQueryIntegrationTests {
 		assertEquals(expected, query, false);
 	}
 
+	@Test // #3072
+	@DisplayName("should build sort object with correct field names")
+	void shouldBuildSortObjectWithCorrectFieldNames() throws NoSuchMethodException, JSONException {
+
+		String methodName = "findByNameOrderBySortAuthor_SortName";
+		Class<?>[] parameterClasses = new Class[] { String.class };
+		Object[] parameters = new Object[] { BOOK_TITLE };
+
+		String query = getQueryString(methodName, parameterClasses, parameters);
+
+		String expected = """
+
+				{
+				              "query": {
+				                "bool": {
+				                  "must": [
+				                    {
+				                      "query_string": {
+				                        "query": "Title",
+				                        "fields": [
+				                          "name"
+				                        ]
+				                      }
+				                    }
+				                  ]
+				                }
+				              },
+				              "sort": [
+				                {
+				                  "sort_author.sort_name": {
+				                    "order": "asc"
+				                  }
+				                }
+				              ]
+				            }""";
+
+		assertEquals(expected, query, false);
+	}
+
 	private String getQueryString(String methodName, Class<?>[] parameterClasses, Object[] parameters)
 			throws NoSuchMethodException {
 
@@ -726,6 +766,8 @@ public abstract class ElasticsearchPartQueryIntegrationTests {
 
 		List<Book> findByAvailableTrueOrderByNameDesc();
 
+		List<Book> findByNameOrderBySortAuthor_SortName(String name);
+
 	}
 
 	public static class Book {
@@ -734,6 +776,10 @@ public abstract class ElasticsearchPartQueryIntegrationTests {
 		@Nullable private String name;
 		@Nullable private Integer price;
 		@Field(type = FieldType.Boolean) private boolean available;
+
+		// this is needed for the #3072 test
+		@Nullable
+		@Field(name = "sort_author", type = FieldType.Object) private Author sortAuthor;
 
 		@Nullable
 		public String getId() {
@@ -766,8 +812,32 @@ public abstract class ElasticsearchPartQueryIntegrationTests {
 			return available;
 		}
 
+		@Nullable
+		public Author getSortAuthor() {
+			return sortAuthor;
+		}
+
+		public void setSortAuthor(@Nullable Author sortAuthor) {
+			this.sortAuthor = sortAuthor;
+		}
+
 		public void setAvailable(Boolean available) {
 			this.available = available;
+
+		}
+	}
+
+	public static class Author {
+		@Nullable
+		@Field(name = "sort_name", type = FieldType.Keyword) private String sortName;
+
+		@Nullable
+		public String getSortName() {
+			return sortName;
+		}
+
+		public void setSortName(@Nullable String sortName) {
+			this.sortName = sortName;
 		}
 	}
 }
